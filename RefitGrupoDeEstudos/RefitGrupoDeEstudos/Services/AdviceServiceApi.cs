0d5f58399d112fd42eed37096a0aa4fd44242013
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2024-01-11 16:32:48 -0300

    Update AdviceServiceApi.cs

--- RefitGrupoDeEstudos/RefitGrupoDeEstudos/Services/AdviceServiceApi.cs
+++ RefitGrupoDeEstudos/RefitGrupoDeEstudos/Services/AdviceServiceApi.cs
@@ -16,16 +16,16 @@ namespace RefitGrupoDeEstudos.Services
             if(string.IsNullOrEmpty(slipId))
                 throw new ArgumentNullException(nameof(slipId));
             
-            var advice = await _httpClient.GetAsync($"/{slipId}");
+            var advice = await _httpClient.GetAsync($"advice/{slipId}");
 
-            return advice.Content.ReadAsStringAsync();
+            return advice.Content.ReadFromJsonAsync<dynamic>().Result;
         }
 
         public async Task<dynamic> GetAsync()
         {
             var advice = await _httpClient.GetAsync("");
             
-            return await advice.Content.ReadAsStringAsync();
+            return advice.Content.ReadFromJsonAsync<dynamic>().Result;
         }
     }
 }
