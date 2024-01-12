6cbae869f8028a45f83ecf3be7d177d022a3eb16
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2024-01-12 08:30:04 -0300

    add refit benchmark

---
+++ RefitBenchmark/MyRefitBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using Refit;
+using RefitGrupoDeEstudos.Controllers;
+using RefitGrupoDeEstudos.Interfaces.External;
+using RefitGrupoDeEstudos.Services;
+
+namespace RefitBenchmark
+{
+    public class MyRefitBenchmark
+    {
+        private readonly IHttpClientFactory _httpClientFactoryService;
+        private readonly IHttpClientFactory _httpClientFactoryRefit;
+        private readonly AdviceControllerWithRefit _refitController;
+        private readonly AdviceControllerWithoutRefit _serviceController;
+
+        public MyRefitBenchmark()
+        {
+            // Inicializar REFIT
+            var refitHttpClient = _httpClientFactoryRefit.CreateClient();
+            refitHttpClient.BaseAddress =  new Uri("https://api.adviceslip.com/advice");
+            var myApi = RestService.For<IAdviceApi>(refitHttpClient);
+            _refitController = new AdviceControllerWithRefit(myApi);
+
+            // Inicializar Service
+            var serviceHttpClient = _httpClientFactoryService.CreateClient();
+            serviceHttpClient.BaseAddress = new Uri("https://api.adviceslip.com/advice");
+            var adviceService = new AdviceServiceApi(serviceHttpClient);
+            _serviceController = new AdviceControllerWithoutRefit(adviceService);
+        }
+
+        [Benchmark]
+        public async Task RefitBenchmarkAsync()
+        {
+            // Chame métodos que utilizam Refit aqui
+            await _refitController.Get();
+        }
+
+        [Benchmark]
+        public async Task ServiceBenchmarkAsync()
+        {
+            // Chame métodos que utilizam a service diretamente aqui
+            await _serviceController.Get();
+        }
+    }
+}
